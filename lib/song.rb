971c85826632a713acaef1c046a5f54d4cfb19b6
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-28 03:05:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,25 @@ def Song
   attr_accessor :name, :artist, :genre
   
   def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
   end
     
+end
+
+class Person
+ 
+  def name=(persons_name)
+    @name = persons_name
+  end
+ 
+  def name
+    @name
+  end
+  
+  def job=(persons_job)
+    @job = persons_job
+  end
+ 
+  def job
+    @job
+  end
 end
