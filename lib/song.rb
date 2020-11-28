badc1340a400fc64925f4a655f2bf3479cf5f40a
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-28 03:09:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,12 @@
 require 'pry'
 
-class Song
+def Song
   attr_accessor :name, :artist, :genre
-
-  @@count = 0
-  @@artists = []
-  @@genres = []
-
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count +=1
-    @@genres << genre
-    @@artists << artist
   end
     
-end
-
-class Dog
- attr_accessor :name, :owner
- 
- def initialize(name)
-   @name = name
- end
-
- def bark
-   "Woof!"
- end
-
- def get_adopted(owner_name)
-   self.owner = owner_name
- end
-
 end
