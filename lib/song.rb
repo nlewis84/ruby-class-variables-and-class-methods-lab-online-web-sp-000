ab370db5c9a1a74aa4d7d0cba3c704d24a2e57ac
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-28 03:08:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,19 @@
 require 'pry'
 
-def Song
+class Song
   attr_accessor :name, :artist, :genre
-  
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
   end
     
 end
