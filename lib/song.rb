05c4e9b3bc42d218fa8a6a65890ae3e1b171d394
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-28 03:44:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,8 @@ class Song
   
   def self.genre_count
     Song.genres.each do |genre|
-      @genre_total = { genre => @@genres.count(genre) }
+      genre_total = { genre => @@genres.count(genre) }
       binding.pry
-      @genre_totals << @genre_total
     end
   end
 end
